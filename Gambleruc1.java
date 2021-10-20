b99166f7d72362f57a5397c846251c18c4445c2b
Renuka13599 <91589849+Renuka13599@users.noreply.github.com>
2021-10-20 09:25:49 +0530

    Add files via upload

---
+++ Gambleruc1.java
@@ -0,0 +1,16 @@
+public class GamblerUc1 {
+    public static void main(String[] args) {
+        System.out.println("**Welcome To Gambling**");
+        GamblerUc1 g = new GamblerUc1();
+        g.gambler();
+    }
+    void gambler(){
+        int stakePerDay = 100;//Stake of gambler
+        int bet = 1;//ber per game
+        System.out.println(+stakePerDay);
+        System.out.println(+bet);
+	
+    }
+}
+
+
